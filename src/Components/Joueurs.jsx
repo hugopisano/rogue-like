09cdacfb6144dec25de9605381c216f63495bceb
Hugo Pisano <hugopisano@MacBook-Air-de-Hugo.local>
2023-11-20 14:40:12 +0100

    Fix propType for Joueurs.jsx file

--- src/Components/Joueurs.jsx
+++ src/Components/Joueurs.jsx
@@ -13,4 +13,11 @@ const Joueurs = ({ data }) => {
     )
 }
 
+Joueurs.propTypes = {
+    data: PropTypes.shape({
+        pv: PropTypes.number.isRequired, // Validation pour data.pv
+        attack: PropTypes.number.isRequired // Validation pour data.attack
+    }).isRequired
+};
+
 export default Joueurs
