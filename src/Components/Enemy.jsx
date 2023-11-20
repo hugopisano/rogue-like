6deb097859f224293cfe4629e6b766176c66cf2a
Hugo Pisano <hugopisano@MacBook-Air-de-Hugo.local>
2023-11-20 14:38:04 +0100

    Fix bug Enemy.js and Joueurs.js

--- src/Components/Enemy.jsx
+++ src/Components/Enemy.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Enemy = ({ data }) => {
     return (
         <div>
@@ -5,6 +7,13 @@ const Enemy = ({ data }) => {
             <p>Ennemi Attaque: {data.attack}</p>
         </div>
     );
+};
+
+Enemy.propTypes = {
+    data: PropTypes.shape({
+        pv: PropTypes.number.isRequired, // Validation pour data.pv
+        attack: PropTypes.number.isRequired // Validation pour data.attack
+    }).isRequired
 };
 
 export default Enemy;
